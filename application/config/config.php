fa2c6039b7c51be4c6b3a9ff604916fb1a348784
Pedro Pinhão <pedro.pinhao@gmail.com>
2011-09-19 23:18:14 +0100

    update host

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://giftweb.lan/';
+$config['base_url']	= 'http://giftweb.local/';
 
 /*
 |--------------------------------------------------------------------------
